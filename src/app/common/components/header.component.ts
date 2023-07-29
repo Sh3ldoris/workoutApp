e828ac79d97a75bfd50af5b70cb70ea0172de79e
Adam Lány <adam.lany@posam.sk>
2023-07-29 16:11:10 +0200

    Header back action update

--- src/app/common/components/header.component.ts
+++ src/app/common/components/header.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { HeaderService } from '../services/header.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { BackLocation } from '../objects/back-location.model';
 
 @Component({
     selector: 'app-header',
     templateUrl: 'header.component.html',
 })
 export class HeaderComponent {
+    backLocation$: Observable<BackLocation | null> = this.headerService.getBackLocation();
     isBackButtonVisible$: Observable<boolean> = this.headerService.isBackButton();
     isHeaderVisible$: Observable<boolean> = this.headerService.isHeaderVisible();
     headerLabel$: Observable<string> = this.headerService.getLabel();
+    constructor(private headerService: HeaderService, private router: Router) {}
 
-    constructor(private headerService: HeaderService) {}
+    goBack(location: BackLocation): void {
+        this.router.navigate([location.url]);
+    }
 }
--- src/app/common/services/header.service.ts
+++ src/app/common/services/header.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { BackLocation } from '../objects/back-location.model';
 
 @Injectable({
     providedIn: 'root',
 })
 export class HeaderService {
+    private backLocation: BehaviorSubject<BackLocation | null> = new BehaviorSubject<BackLocation | null>(null);
     private isBackButtonVisible: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
     private isVisible: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
     private headerLabel: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+    resetHeader(): void {
+        this.backLocation.next(null);
+        this.isBackButtonVisible.next(false);
+        this.isVisible.next(false);
+        this.headerLabel.next('');
+    }
 
     isBackButton(): Observable<boolean> {
         return this.isBackButtonVisible;
@@ -35,5 +44,13 @@ export class HeaderService {
 
     getLabel(): Observable<string> {
         return this.headerLabel;
+    }
+
+    setBackLocation(location: BackLocation): void {
+        this.backLocation.next(location);
+    }
+
+    getBackLocation(): Observable<BackLocation | null> {
+        return this.backLocation;
     }
 }
